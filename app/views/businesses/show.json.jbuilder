32991cc6559d710360b208e28f336f0fdb28a758
Ale <alealt94@hotmail.com>
2014-10-11 14:08:27 +1100

    added businesses

---
+++ app/views/businesses/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @business, :id, :name, :tagline, :decription, :url, :owner_id, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141010234117) do
+ActiveRecord::Schema.define(version: 20141011030404) do
+
+  create_table "businesses", force: true do |t|
+    t.string   "name"
+    t.string   "tagline"
+    t.text     "decription"
+    t.string   "url"
+    t.integer  "owner_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "friendly_id_slugs", force: true do |t|
     t.string   "slug",                      null: false
